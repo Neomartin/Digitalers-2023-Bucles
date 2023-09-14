4435da246db7f5cc0efc2f23a660cb65169e2550
Martin Rodriguez <neomartinr@gmail.com>
2023-09-14 20:19:11 -0300

    Do while finalizado

--- js/do-while.js
+++ js/do-while.js
@@ -1,14 +1,17 @@
-let edadCliente = parseInt(    prompt("Ingrese edad de cliente")     ); //NaN
+let edadCliente
 
+do {
 
-while( isNaN(edadCliente) || edadCliente > 120 || edadCliente  <= 0   ) {
-
-
-    // Le vuelvo a pedir que haga un prompt
     edadCliente = parseInt(prompt("Ingrese edad de cliente"));
 
-    
-}
+} while( isNaN(edadCliente) || edadCliente > 120 || edadCliente  <= 0   )
+
+
+
+// while( isNaN(edadCliente) || edadCliente > 120 || edadCliente  <= 0   ) {
+//     // Le vuelvo a pedir que haga un prompt
+//     edadCliente = parseInt(prompt("Ingrese edad de cliente"));
+// }
 
 
 
